3985ee9b23cdfe3bcde0dfc8d4be0c772ae20e92
PierreGallinMartel <pierre.gallinmartel@gmail.com>
2023-06-12 12:58:39 +0200

    final revision

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Phase 4 :
 - Internet services OK
         PE routers can now also handle standard internet, meaning they don't necessarily have vrfs. The adjAS matrix handles the border router connections.
 - Ingress TE multi-connected CE routers OK
-        each PE associates a MED and a BGP community to the routes it shares with its client, allowing the latter to modify those and perform TE based on them
+        each PE associates a MED and a BGP community to the routes it shares with its client, allowing the latter to modify those and perform TE based on them. The community is always AsNumber:RouterNumber, and the MED is defined in the json file (see "PE" structure).
 - Site sharing OK
         route target imports are handled by the "Clients" structure. The self import is always implicit, other imports can be made to connect different VPN clients.
 
@@ -29,11 +29,11 @@ The port list is used for Telnet to specify where to send the configs.
 
 The PE list is organized as follows : [[router_1, [VRF1], [VRF2], ...], [router_2, [VRF1], [VRF2], ...]]. Each PE can have several VRFs **or none** (Internet Services, necessary in order to open BGP sesson with Route Reflector).
 
-The VRFs are defined as : [interface_name, vrf_name, route_target, net_ip@, ce_ip@, client_as_number, med_client]
+The VRFs are defined as : [interface_name, vrf_name, route_target, net_ip@, ce_ip@, client_as_number, med_PE]
 
 Each client can communicate with a list of other clients : "Client A" can communicate with [["Client_B", route_target_client_b], ["Client_C", route_target_client_c]]
 
-Finally adjAS is an adjacency matrix used if we have several AS. For four AS, it would look like this : 
+Finally adjAS is an adjacency matrix used if we have several AS connected by regular internet. For four AS, it would look like this : 
 
 "adjAS": 
 
@@ -58,3 +58,7 @@ To modify the inside matrix of an AS in the JSON, type :
 * python3 modif_matrix.py _AS_number_of_the_matrix_to_modify_
 
 You'll then be able to add/delete routers and create connections between them. The resulting matrix will be printed in the terminal, and all you have to do is copy paste it in the JSON file. 
+
+**Disclaimer:
+
+The client configs weren't generated automatically as it was not the aim of this project, however they can be easily deduced from the vrf configs on the PE routers
---
+++ test_main.py
@@ -0,0 +1,34 @@
+import unittest
+
+import main
+
+class MainTest(unittest.TestCase):
+
+    def test_collapse_parsed_data(self):
+        lst = ['end', ['configure terminal', ['router bgp 1', 'neighbor 1.1.1.1 ...', ['address-family ipv6', 'neighbor ...', ['router bgp 2', 'neighbor 1.1.1.1 ...', 'neighbor 2.2.2.2 ...', 'exit'], ['vrf definition blabla', 'test', 'exit'], 'exit'], ['address-family enplus', 'neighbor ...', 'exit'], ['address-family ipv6', 'neighbor 555', 'exit'], 'end']], ['configure terminal', ['router bgp 1', 'neighbor 4.4.4.4 ...', 'exit'], 'exit']]
+        self.assertEqual(main.collapse_parsed_data(lst), ['end', ['configure terminal', ['router bgp 1', 'neighbor 1.1.1.1 ...', ['address-family ipv6', 'neighbor ...', ['router bgp 2', 'neighbor 1.1.1.1 ...', 'neighbor 2.2.2.2 ...', 'exit'], ['vrf definition blabla', 'test', 'exit'], 'neighbor 555', 'exit'], ['address-family enplus', 'neighbor ...', 'exit'], 'neighbor 4.4.4.4 ...', 'end']]])
+
+
+    def test_parse_cfg_data(self):
+        data = ["end", "router bgp 1", "neighbor 1.1.1.1 ...", "address-family ipv6", "neighbor ...", "router bgp 2", "neighbor 1.1.1.1 ...", "neighbor 2.2.2.2 ...", "exit", "vrf definition blabla", "test", "exit", "exit", "address-family enplus", "neighbor ...", "exit", "exit", "router bgp 1", "neighbor 4.4.4.4 ...", "exit"]
+        self.assertEqual(main.parse_cfg_data(data), ["end", ["router bgp 1", "neighbor 1.1.1.1 ...", ["address-family ipv6", "neighbor ...", ["router bgp 2", "neighbor 1.1.1.1 ...", "neighbor 2.2.2.2 ...", "exit"], ["vrf definition blabla", "test", "exit"], "exit"], ["address-family enplus", "neighbor ...", "exit"], "neighbor 4.4.4.4 ...", "exit"]])
+
+
+    def test_compare_cfg_data(self):
+        old_parsed_data = ['end', 'enable', ['configure terminal', ['router ospf 1', 'mpls ldp autoconfig', 'router-id 1.0.0.1', 'exit'], 'mpls ip', ['interface FastEthernet0/0', 'ip address 1.0.1.1 255.255.255.248', 'ip ospf cost 10', 'no shutdown', 'ip ospf 1 area 0', 'mpls ip', 'exit'], ['router bgp 1', 'neighbor 3.3.3.3 remote-as 1', 'neighbor 3.3.3.3 update-source Loopback0', ['address-family vpnv4', 'neighbor 3.3.3.3 activate', 'neighbor 3.3.3.3 send-community both', 'exit-address-family'], 'end']]]
+        new_parsed_data = ['end', 'enable', ['configure terminal', ['router ospf 1', 'mpls ldp autoconfig', 'router-id 1.0.0.1', 'exit'], 'mpls ip', ['interface GigabitEthernet0/0', 'ip address 1.0.1.1 255.255.255.248', 'ip ospf cost 10', 'no shutdown', 'ip ospf 1 area 0', 'mpls ip', 'exit'], ['router bgp 1', 'neighbor 3.3.3.3 remote-as 1', 'neighbor 3.3.3.3 update-source Loopback0', ['address-family vpnv4', 'neighbor 4.4.4.4 activate', 'neighbor 3.3.3.3 send-community both', 'exit-address-family'], '', 'end']]]
+
+        added, deleted = main.compare_cfg_data(old_parsed_data, new_parsed_data)
+        added = main.magic_replace_end(added)
+        deleted = main.magic_replace_end(deleted)
+
+        self.assertEqual(added, ['enable', ['configure terminal', ['router bgp 1', ['address-family vpnv4', 'neighbor 4.4.4.4 activate', 'exit-address-family'], 'exit'], ['interface GigabitEthernet0/0', 'ip address 1.0.1.1 255.255.255.248', 'ip ospf cost 10', 'no shutdown', 'ip ospf 1 area 0', 'mpls ip', 'end']]])
+        self.assertEqual(deleted, ['enable', ['configure terminal', ['interface FastEthernet0/0', 'ip address 1.0.1.1 255.255.255.248', 'ip ospf cost 10', 'no shutdown', 'ip ospf 1 area 0', 'mpls ip', 'exit'], ['router bgp 1', ['address-family vpnv4', 'neighbor 3.3.3.3 activate', 'exit-address-family'], 'end']]])
+
+
+    def test_magic_replace_end(self):
+        lst = ['enable', ['configure terminal', ['router bgp 1', 'neighbor 1.1.1.1 ...', 'end'], ['vrf definition Client_A', 'test', 'exit']], ['configure terminal', ['vrf definition Client_B', 'test', 'end'], ['bgp router 2', 'blabla', 'end']]]
+        self.assertEqual(main.magic_replace_end(lst), ['enable', ['configure terminal', ['router bgp 1', 'neighbor 1.1.1.1 ...', 'exit'], ['vrf definition Client_A', 'test', 'end']], ['configure terminal', ['vrf definition Client_B', 'test', 'exit'], ['bgp router 2', 'blabla', 'end']]])
+
+if __name__ == "__main__":
+    unittest.main()
